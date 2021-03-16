45581bf911ddfd1d72684a42eaeb9a7a2c91111f
Luiz Chagas <luiz.chagas@veplex.com.br>
2021-03-16 06:16:32 -0300

    Commit inicial

---
+++ src/main/java/br/edu/ifsul/controle/ControlePessoa.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.controle;
+
+import br.edu.ifsul.dao.PessoaDao;
+import br.edu.ifsul.model.Pessoa;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Pichau
+ */
+@Named(value = "controlePessoa")
+@ViewScoped
+public class ControlePessoa implements Serializable {
+
+    @EJB
+    private PessoaDao<Pessoa> dao;
+    private Pessoa objeto;
+
+    public ControlePessoa() {
+
+    }
+
+    public String listar() {
+        return "/privado/pessoa/listar?faces-redirect=true";
+    }
+
+    public void novo() {
+        objeto = new Pessoa();
+    }
+
+    public void alterar(Object id) {
+        try {
+            objeto = dao.getObjectById(id);
+        } catch (Exception e) {
+            Util.mensagemInformacao("Erro ao recuperar objeto: " + Util.getMensagemErro(e));
+        }
+    }
+
+    public void excluir(Object id) {
+        try {
+            objeto = dao.getObjectById(id);
+            dao.remove(objeto);
+            Util.mensagemInformacao("Objeto removido com sucesso!");
+        } catch (Exception e) {
+            Util.mensagemInformacao("Erro ao remover objeto: " + Util.getMensagemErro(e));
+        }
+    }
+
+    public void salvar() {
+        try {
+            if (objeto.getId() == null) {
+                dao.persist(objeto);
+            } else {
+                dao.merge(objeto);
+            }
+            Util.mensagemInformacao("Objeto persistido com sucesso!");
+        } catch (Exception e) {
+            Util.mensagemInformacao("Erro ao salvar objeto: " + Util.getMensagemErro(e));
+        }
+    }
+
+    public PessoaDao<Pessoa> getDao() {
+        return dao;
+    }
+
+    public void setDao(PessoaDao<Pessoa> dao) {
+        this.dao = dao;
+    }
+
+    public Pessoa getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(Pessoa objeto) {
+        this.objeto = objeto;
+    }
+
+}
